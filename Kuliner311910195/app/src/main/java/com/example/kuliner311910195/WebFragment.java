c0694310ff358e5c2a3eaafb832f214938370fc3
ikmalriyan21 <56243809+ikmalriyan21@users.noreply.github.com>
2022-07-13 14:00:49 +0700

    Add files via upload

---
+++ Kuliner311910195/app/src/main/java/com/example/kuliner311910195/WebFragment.java
@@ -0,0 +1,28 @@
+package com.example.kuliner311910195;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class WebFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_web, container, false);
+        WebView myWebView = myView.findViewById(R.id.wv_web);
+        myWebView.loadUrl("https://www.google.com");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Inflate the layout for this fragment
+        return myView;
+    }
+}
